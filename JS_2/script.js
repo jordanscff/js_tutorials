20f0c062e065c17546f94a2715174e5670028c9a
jordanscff <54541513+jordanscff@users.noreply.github.com>
2021-06-18 14:57:37 +0100

    Add files via upload

--- JS_2/script.js
+++ JS_2/script.js
@@ -7,3 +7,15 @@ function describeCountry(country, population, capitalCity) {
 console.log(describeCountry('Scotland', 5500000, 'Edinburgh'));
 console.log(describeCountry('England', 65000000, 'London'));
 console.log(describeCountry('Wales', 3500000, 'Cardiff'));
+
+let worldPopulation = 7900000000;
+
+function percentageOfWorld1 (population) {
+  return population / worldPopulation * 100;
+}
+console.log(percentageOfWorld1(5500000));
+
+let percentageOfWorld2 = function (population) {
+  return population / worldPopulation * 100;
+}
+console.log(percentageOfWorld2(5500000));
