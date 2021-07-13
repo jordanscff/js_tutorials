527ce093980076b9bad389493ed9d0c967523250
jordanscff <54541513+jordanscff@users.noreply.github.com>
2021-07-13 17:13:09 +0100

    Add files via upload

--- JS_4/script.js
+++ JS_4/script.js
@@ -1,9 +1,22 @@
 'use strict';
 
-document.querySelector('.check').addEventListener('click', function() {
-    const guess = Number(document.querySelector('.guess').value);
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = secretNumber;
+let score = 20;
+document.querySelector('.score').textContent = score;
 
-    if(!guess){
-    document.querySelector('.message').textContent = 'No number! Enter number'
-    }
-})
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No number! Enter number';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = 'Correct Number';
+  } else if (guess > secretNumber) {
+    document.querySelector('.score').textContent = score--;
+    document.querySelector('.message').textContent = 'Too high!';
+  } else if (guess < secretNumber) {
+    document.querySelector('.score').textContent = score--;
+    document.querySelector('.message').textContent = 'Too low!';
+  }
+});
