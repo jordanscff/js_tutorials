945026622846f251dd1ad6fcd3d6f222bc341a4d
jordanscff <54541513+jordanscff@users.noreply.github.com>
2021-07-14 16:00:43 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -11,6 +11,8 @@ document.querySelector('.check').addEventListener('click', function () {
   if (!guess) {
     document.querySelector('.message').textContent = 'No number! Enter number';
   } else if (guess === secretNumber) {
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
     document.querySelector('.message').textContent = 'Correct Number';
   } else if (guess > secretNumber) {
     if (score > 0) {
