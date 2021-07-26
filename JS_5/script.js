10c3ba48933d67512864c2728b3af14804b5db54
jordanscff <54541513+jordanscff@users.noreply.github.com>
2021-07-26 14:22:01 +0100

    Add files via upload

--- JS_5/script.js
+++ JS_5/script.js
@@ -5,6 +5,31 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 
+const openModal = function () {
+  console.log('button clicked');
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+//Whenever a click happens, open the modal
 for (let i = 0; i < btnsOpenModal.length; i++) {
-  console.log(btnsOpenModal[i].textContent);
+  btnsOpenModal[i].addEventListener('click', openModal);
 }
+
+//Close the modal when the X is clicked or the overlay is clicked
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+//Listen for events everywhere including keyboards
+document.addEventListener('keydown', function (event) {
+  if (event.key === 'Escape') {
+    if (!modal.classList.contains('hidden')) {
+      closeModal();
+    }
+  }
+});
