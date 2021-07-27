a93df017dac2f6b7fd29d6008c2c2c919d490448
jordanscff <54541513+jordanscff@users.noreply.github.com>
2021-07-27 17:14:50 +0100

    Add files via upload

---
+++ JS_6/script.js
@@ -0,0 +1,9 @@
+'use strict';
+
+let score1 = document.getElementById('score--0');
+let score2 = document.getElementById('score--1');
+let diceEl = document.querySelector('.dice');
+
+score1.textContent = 0;
+score2.textContent = 0;
+diceEl.classList.add('hidden');
