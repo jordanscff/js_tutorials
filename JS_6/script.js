d84226a1ee62bf454bc70a3f82910b7dd93d2bd0
jordanscff <54541513+jordanscff@users.noreply.github.com>
2021-07-30 09:22:04 +0100

    Add files via upload

--- JS_6/script.js
+++ JS_6/script.js
@@ -2,8 +2,31 @@
 
 let score1 = document.getElementById('score--0');
 let score2 = document.getElementById('score--1');
+let current0El = document.getElementById('current--0');
+let current1El = document.getElementById('current--1');
 let diceEl = document.querySelector('.dice');
+let btnNew = document.querySelector('.btn--new');
+let btnRoll = document.querySelector('.btn--roll');
+let btnHold = document.querySelector('.btn--hold');
 
 score1.textContent = 0;
 score2.textContent = 0;
 diceEl.classList.add('hidden');
+
+let currentScore = 0;
+
+btnRoll.addEventListener('click', function () {
+  const diceNumber = getRandomInt(6);
+  diceEl.src = `dice-${diceNumber}.png`;
+  diceEl.classList.remove('hidden');
+  if (diceNumber === 1) {
+    return;
+  } else {
+    currentScore += diceNumber;
+    current0El.textContent = currentScore;
+  }
+});
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max) + 1;
+}
